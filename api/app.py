7ed2ce9f3dd4b843109eb8b024904b9e07b28615
JJ Jin <106864282+JIN-ZIJIE@users.noreply.github.com>
2023-03-17 21:21:39 +0800

    update

--- api/app.py
+++ api/app.py
@@ -13,7 +13,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
+@app.route('/book')
+def book():
+    return render_template('book.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
 ###############################
 ############ ERROR ############
 ###############################
